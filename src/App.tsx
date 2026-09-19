137b2c4ad39049acf066d39119dd0a6ad2772bbc
Rushabh Tech Hub <vajirnathrushabh@gmail.com>
2026-09-19 14:49:15 +0530

    Add Quote component to App

--- src/App.tsx
+++ src/App.tsx
@@ -132,6 +132,7 @@ export default function App() {
         <Services />
         <Portfolio />
         <Blog adminMode={adminMode} />
+        <Quote />
         <Contact adminMode={adminMode} onMessageReceived={updateUnreadCount} />
       </main>
 
